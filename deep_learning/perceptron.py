958ef1ab133d2cf06a63719ed32385705af4b3a8
kazchimo <pharcydetip@gmail.com>
2022-04-04 09:40:56 +0900

    add NAND

--- deep_learning/perceptron.py
+++ deep_learning/perceptron.py
@@ -10,3 +10,14 @@ def AND(x1: float, x2: float) -> int:
     return 0
   else:
     return 1
+
+
+def NAND(x1: float, x2: float) -> int:
+  x = np.array([x1, x2])
+  w = np.array([-0.5, -0.5])
+  b = 0.7
+
+  if np.sum(w * x) + b <= 0:
+    return 0
+  else:
+    return 1
